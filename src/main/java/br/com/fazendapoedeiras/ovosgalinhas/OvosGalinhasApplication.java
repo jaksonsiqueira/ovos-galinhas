17de6927e139be09ca619e027b098f03ad32147e
jaksonpsiqueira <jaksonpsiqueira@gmail.com>
2023-10-12 16:30:34 -0300

    iniciado o projeto

---
+++ src/main/java/br/com/fazendapoedeiras/ovosgalinhas/OvosGalinhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fazendapoedeiras.ovosgalinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvosGalinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvosGalinhasApplication.class, args);
+	}
+
+}
