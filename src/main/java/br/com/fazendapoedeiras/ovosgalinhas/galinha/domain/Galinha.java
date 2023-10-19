57922c89700e3e23da4479e87adfba41e111a930
jaksonpsiqueira <jaksonpsiqueira@gmail.com>
2023-10-19 19:58:59 -0300

    ajusta mapeamento id uuid

--- src/main/java/br/com/fazendapoedeiras/ovosgalinhas/galinha/domain/Galinha.java
+++ src/main/java/br/com/fazendapoedeiras/ovosgalinhas/galinha/domain/Galinha.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,6 +20,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Galinha {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "idGalinha", updatable = false, unique = true, nullable = false)
 	private UUID idGalinha;
 	@NotBlank
 	private String nomeGalinha;
@@ -26,8 +31,7 @@ public class Galinha {
 	private LocalDateTime dataHoraDoCadastro;
 	private LocalDateTime dataHoraDaUltimaAlteracao;
 	
-	public Galinha(UUID idGalinha, @NotBlank String nomeGalinha, @NotNull LocalDate dataDeNascimento) {
-		this.idGalinha = UUID.randomUUID();
+	public Galinha(@NotBlank String nomeGalinha, @NotNull LocalDate dataDeNascimento) {
 		this.nomeGalinha = nomeGalinha;
 		this.dataDeNascimento = dataDeNascimento;
 		this.dataHoraDoCadastro = LocalDateTime.now();
