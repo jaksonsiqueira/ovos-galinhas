0f85c2ee56d22c4b11132e843cba28e3a0369902
jaksonpsiqueira <jaksonpsiqueira@gmail.com>
2023-11-10 16:41:06 -0300

    validando que a galinha existe para registro dos ovos

--- src/main/java/br/com/fazendapoedeiras/ovosgalinhas/ovos/aplication/service/RegistroDeOvosAplicationService.java
+++ src/main/java/br/com/fazendapoedeiras/ovosgalinhas/ovos/aplication/service/RegistroDeOvosAplicationService.java
@@ -4,18 +4,23 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
-
+import br.com.fazendapoedeiras.ovosgalinhas.galinha.application.service.GalinhaService;
 import br.com.fazendapoedeiras.ovosgalinhas.ovos.aplication.api.RegistroDeOvosRequest;
 import br.com.fazendapoedeiras.ovosgalinhas.ovos.aplication.api.RegistroDeOvosResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class RegistroDeOvosAplicationService implements RegistroDeOvosService {
-
+	private final GalinhaService galinhaService;
+	
 	@Override
 	public RegistroDeOvosResponse criaRegistroDeOvos(UUID idGalinha, RegistroDeOvosRequest registroDeOvosRequest) {
 		log.info("[inicia]RegistroDeOvosAplicationService - criaRegistroDeOvos ");
+		galinhaService.buscaGalinhaPorId(idGalinha);
+		
 		log.info("[finaliza]RegistroDeOvosAplicationService - criaRegistroDeOvos ");
 		return null;
 	}
